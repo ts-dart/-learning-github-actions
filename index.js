82c754bb3a1176806b202cd2653a9b82f3aa7d6d
Thiago Henrique <thiagoedusan5.11@outlook.com>
2022-07-22 20:41:58 -0300

    agora?

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send(`<h1> Executando na porta: ${port} </h1>`));
-app.listen(port, () => console.log(`Online: ${port}`));
+app.get('/', (req, res) => res.send(`<h1> Executando na porta: ${PORT} </h1>`));
+
+app.listen(PORT, () => console.log(`Online: ${PORT}`));
