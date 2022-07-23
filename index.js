c64a6cb65360066f11e28b72cfb464be4846aaf7
Thiago Henrique <thiagoedusan5.11@outlook.com>
2022-07-22 21:01:23 -0300

    atualizando

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send(`<h1> Executando na porta: ${PORT} </h1>`));
+app.get('/', (req, res) => res.send(`<h1> Online na porta: ${PORT} </h1>`));
 
 app.listen(PORT, () => console.log(`Online: ${PORT}`));
